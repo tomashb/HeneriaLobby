d92bf5226d86699323de8046a28a88192ccac691
tomashb <62190147+tomashb@users.noreply.github.com>
2025-09-21 19:43:33 +0200

    Add missing Timestamp import in GroupManager

--- src/main/java/com/lobby/social/groups/GroupManager.java
+++ src/main/java/com/lobby/social/groups/GroupManager.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 import java.sql.ResultSetMetaData;
 import java.util.HashMap;
 import java.util.Locale;
